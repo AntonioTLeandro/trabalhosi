652232ee76228e70de53336b586ca30bf41165f7
AntonioTLeandro <47145799+AntonioTLeandro@users.noreply.github.com>
2019-11-10 21:24:37 -0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 $("#menu").mouseenter(function (e) {
-    $("#menu").css({"width":"300px", "height": "200px"})
+    $("#menu").css({"width":"300px", "height": "210px"})
     $("#menu_options").css({"display":"block"})
 })
 $("#menu").mouseleave(function (e) {
